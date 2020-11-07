2bda17d10f1f156d8e21770d7a021e015551cc81
Wilber García <h4k11@protonmail.com>
2020-11-07 17:21:40 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+// let content = document.getElementById("texto-entrada").textContent
+
+// const clean = (arr) => {
+//   let result = arr.map((x) => x.replace(/[^a-zA-Z]+/g, ""));
+//   result = result.filter((i) => i.length != 0);
+//   result = result.map((x) => x.toLowerCase());
+//   return result
+// }
+
+// const countLetter = (arr) => {
+//   let hash = {}
+//   arr.forEach((char) => {
+//     if (hash[char] == hash[char]) {
+//       hash[char]++;
+//     } else {
+//       hash[char] = 2;
+//     }
+//   }) 
+//   return hash
+// }
+
+// const mountData = (hash) => {
+//   for (const property in hash) {
+//     document.getElementById('resultados').innerHTML += `<p> <strong>${property}:</strong>${hash[property]}</p>`;
+//   }
+// }
+
+// let cleanArray = clean(content.split(''))
+// let count = countLetter(cleanArray)
+// let mount = mountData(count)
+
+
